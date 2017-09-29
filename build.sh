4a547841c8fe63fccdaf42e651894eb933600594
cyg2009 <cygtest2015@gmail.com>
2017-09-29 11:54:31 -0700

    Fix bug of ev in build script.

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 umask 027
-docker run -it -v $(pwd):/go/src golang:1.9 sh -c "go get github.com/cyg2009/MyTestCode && cd /go/src/github.com/cyg2009/MyTestCode && CGO_ENABLED=0
- GO_EXTLINK_ENABLED=0  go build --ldflags '-extldflags "-static"' -a  -o /go/src/processrouter && rm -fr /go/src/github.com"
+docker run -it -v $(pwd):/go/src golang:1.9 sh -c "go get github.com/cyg2009/MyTestCode && cd /go/src/github.com/cyg2009/MyTestCode && export CGO_ENABLED=0 && export GO_EXTLINK_ENABLED=0 && go build --ldflags '-extldflags "-static"' -a  -o /go/src/processrouter && rm -fr /go/src/github.com"
 
 echo To build the docker image: 
 echo docker build -t my:1.0 .
