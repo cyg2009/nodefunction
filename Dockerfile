eae7ab9832f440027e30d7bcf7e4f3870e1e0f81
cyg2009 <cygtest2015@gmail.com>
2017-09-29 11:57:33 -0700

    change binary name to funcRouter

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,17 @@
 #This is a debian flavor of our lambda base image.
 
-FROM gcr.io/k8stest-167418/alpine-node:272212
+#FROM gcr.io/k8stest-167418/alpine-node:272212
+FROM mhart/alpine-node:8
 RUN mkdir -p /var/runtime 
 ADD runtime /var/runtime
-ADD processrouter /var/runtime/bin/processrouter
+ADD funcRouter /var/runtime/bin/funcRouter
 WORKDIR /var/runtime/bin
 ENV PATH="/var/runtime/bin:$PATH" \
-    DEFAULT_SERVER_PORT="28903" \
+    DEFAULT_SERVER_PORT="8903" \
     RUNTIME_ROOT="/var/runtime" \
     RUNTIME_LAMBDA="/var/runtime/bin/lambda-run"
 
-EXPOSE 28903
+EXPOSE 8903
 
-CMD ["/var/runtime/bin/processrouter"]
+CMD ["/var/runtime/bin/funcRouter"]
 
--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 umask 027
-docker run -it -v $(pwd):/go/src golang:1.9 sh -c "go get github.com/cyg2009/MyTestCode && cd /go/src/github.com/cyg2009/MyTestCode && export CGO_ENABLED=0 && export GO_EXTLINK_ENABLED=0 && go build --ldflags '-extldflags "-static"' -a  -o /go/src/processrouter && rm -fr /go/src/github.com"
+docker run -it -v $(pwd):/go/src golang:1.9 sh -c "go get github.com/cyg2009/MyTestCode && cd /go/src/github.com/cyg2009/MyTestCode && export CGO_ENABLED=0 && export GO_EXTLINK_ENABLED=0 && go build --ldflags '-extldflags "-static"' -a  -o /go/src/funcRouter && rm -fr /go/src/github.com"
 
 echo To build the docker image: 
 echo docker build -t my:1.0 .
